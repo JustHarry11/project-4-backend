c28327d65c205f00f0e7dc36e2f687bcc3a5b5fb
Harry <harrylippa@gmail.com>
2025-06-04 13:15:39 +0100

    TESTING

--- boardgames/views.py
+++ boardgames/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 class BoardgameListView(APIView):
     # Index
     def get(self, request):
-        return Response('HITTING INDEX ROUTE')
+        return Response('HIT INDEX ROUTE')
 
     # Create
     def post(self, request):
