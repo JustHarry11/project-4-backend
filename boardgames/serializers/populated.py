139b88691afe97ea80cbac74ced9ccbf24be2917
Harry <harrylippa@gmail.com>
2025-06-05 09:27:47 +0100

    Put the username in the owner fieldd of Index and Show pages

---
+++ boardgames/serializers/populated.py
@@ -0,0 +1,5 @@
+from .common import BoardgameSerializer
+from users.serializers.common import UsernameSerializer
+
+class PopulatedBoardgameSerializer(BoardgameSerializer):
+    owner = UsernameSerializer()
