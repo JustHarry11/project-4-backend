def656a1378044e0b4e84dda352eb759cfb2d144
Harry <harrylippa@gmail.com>
2025-06-05 15:23:49 +0100

    Added seed and username to likes

--- boardgames/serializers/populated.py
+++ boardgames/serializers/populated.py
@@ -3,3 +3,4 @@ from users.serializers.common import UsernameSerializer
 
 class PopulatedBoardgameSerializer(BoardgameSerializer):
     owner = UsernameSerializer()
+    likes = UsernameSerializer(many=True)
