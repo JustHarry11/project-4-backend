f9475de36c92513efe8b4284807b2b5bf09e7a69
Harry <harrylippa@gmail.com>
2025-06-11 17:08:26 +0100

    made the fields required

--- boardgames/serializers/common.py
+++ boardgames/serializers/common.py
@@ -5,3 +5,13 @@ class BoardgameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Boardgame
         fields = '__all__'
+        extra_kwargs = {
+            'title': {'required': True},
+            'description': {'required': True},
+            'instruction': {'required': True},
+            'image_url': {'required': True},
+            'type': {'required': True},
+            'genre': {'required': True},
+            'min_players': {'required': True},
+            'max_players': {'required': True},
+        }
